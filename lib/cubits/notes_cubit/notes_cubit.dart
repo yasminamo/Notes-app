51dde2122d0e5dbcc44052d5bbcccee5e450e6b2
Yasmin Mohamed <yaso.mohamed0987@gmail.com>
2024-11-17 19:47:04 +0200

    create fetch notes method

--- lib/cubits/notes_cubit/notes_cubit.dart
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -1,9 +1,21 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
+import 'package:note_app/constans.dart';
 import 'package:note_app/models/note_model.dart';
-
 part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
+  fetchAllNotes() async {
+    emit(NotesLoading());
+    try {
+      var notesBox = Hive.box<NoteModel>(kNotesBox);
+      List<NoteModel> notes = notesBox.values.toList();
+
+      emit(NotesSuccess(notes));
+    } catch (e) {
+      emit(NotesFailuer(e.toString()));
+    }
+  }
 }
--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_app/cubits/notes_cubit/notes_cubit.dart';
 import 'package:note_app/views/Widgets/add_note_bottom_sheet.dart';
 import 'package:note_app/views/Widgets/notes_view_body.dart';
 
@@ -7,22 +9,25 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showModalBottomSheet(
-              isScrollControlled: true,
-              context: context,
-              builder: (context) {
-                return const AddNoteBottomSheet();
-              });
-        },
-        backgroundColor: Colors.blueGrey,
-        child: const Icon(
-          Icons.add,
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showModalBottomSheet(
+                isScrollControlled: true,
+                context: context,
+                builder: (context) {
+                  return const AddNoteBottomSheet();
+                });
+          },
+          backgroundColor: Colors.blueGrey,
+          child: const Icon(
+            Icons.add,
+          ),
         ),
+        body: const NotesviewBody(),
       ),
-      body: const NotesviewBody(),
     );
   }
 }
